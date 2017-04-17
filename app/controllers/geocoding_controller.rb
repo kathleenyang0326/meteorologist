352bb620a5a51bce54c3341c0848ce90b27ec86e
Kathleen Yang <kathleen.yang@users.noreply.github.com>
2017-04-17 14:31:14 -0500

    streettocoords task updated

--- app/controllers/geocoding_controller.rb
+++ app/controllers/geocoding_controller.rb
@@ -8,6 +8,11 @@ class GeocodingController < ApplicationController
 
   def street_to_coords
     @street_address = params[:user_street_address]
+    fortracking= @street_address.strip.gsub(/[^a-z0-9\s]/i, "+").gsub(" ","+")
+    url="http://maps.googleapis.com/maps/api/geocode/json?address="+fortracking+"&"
+    parsed_data = JSON.parse(open(url).read)
+@latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]
+@longitude = parsed_data["results"][0]["geometry"]["location"]["lng"]
 
     # ==========================================================================
     # Your code goes below.
@@ -17,9 +22,7 @@ class GeocodingController < ApplicationController
 
 
 
-    @latitude = "Replace this string with your answer."
 
-    @longitude = "Replace this string with your answer."
 
     render("geocoding/street_to_coords.html.erb")
   end
